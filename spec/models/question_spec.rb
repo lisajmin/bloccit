dea563745bae9d053622198954aaa0b39bcbae8a
Lisa Min <lisajmin@gmail.com>
2017-04-09 14:44:49 -0700

    Assignment 4: Add Question and Answer models

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: false) }
+
+  describe "attributes" do
+    it "has title, body, resolved attributes" do
+      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body", resolved: false)
+    end
+  end
+
+  it "should respond to title" do
+    expect(question).to respond_to(:title)
+  end
+
+  it "should respond to body" do
+    expect(question).to respond_to(:body)
+  end
+
+  it "should respond to resolve" do
+    expect(question).to respond_to(:resolved)
+  end
+
+end
