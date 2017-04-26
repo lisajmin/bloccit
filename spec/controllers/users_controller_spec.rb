c07e87229a04a1df71cf4fd3983bf16c18c5b5bc
Lisa Min <lisajmin@gmail.com>
2017-04-25 23:06:51 -0700

    Assignment: Create static confirmation HTML page, include Rspec test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -50,4 +50,21 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe "POST confirm" do
+    it "sets user name properly" do
+      post :confirm, user: new_user_attributes
+      expect(assigns(:user).name).to eq new_user_attributes[:name]
+    end
+
+    it "sets user email properly" do
+      post :confirm, user: new_user_attributes
+      expect(assigns(:user).email).to eq new_user_attributes[:email]
+    end
+
+    it "sets user password properly" do
+      post :confirm, user: new_user_attributes
+      expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
+    end
+  end
+
 end
