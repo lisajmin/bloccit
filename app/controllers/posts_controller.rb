9d9934c2846dce023b3211db85ab81fd2b4e316a
Lisa Min <lisajmin@gmail.com>
2017-05-06 21:48:18 -0700

    Assignment 16: Add moderator role

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
 
   before_action :require_sign_in, except: :show
-  before_action :authorize_user, except: [:show, :new, :create]
+  before_action :moderator_user, except: [:show, :new, :create]
+  before_action :authorize_user, except: [:show, :new, :create, :edit, :update]
 
   def show
     @post = Post.find(params[:id])
@@ -59,6 +60,14 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def moderator_user
+    post = Post.find(params[:id])
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
+      flash[:alert] = "You must be an admin or moderator to do that."
+      redirect_to [post.topic, post]
+    end
   end
 
   def authorize_user
