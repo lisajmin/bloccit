6d4ad7aa0f7deaad7acb96b372566ef828ca453d
Lisa Min <lisajmin@gmail.com>
2017-05-07 21:05:45 -0700

    Assignment 19: Add function to automatically follow posts that user has authored

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,14 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to:user.email, subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<post/#{post.id}@Bloccit>"
+    headers["In-Reply-To"] = "<post/#{post.id}@Bloccit>"
+    headers["References"] = "<post/#{post.id}@Bloccit>"
+
+    @post = post
+
+    mail(to:post.user.email, subject: "You're following, #{post.title}!")
+  end
 end
