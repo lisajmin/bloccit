de93c2766d068bcf989f23977e92be3629b06c22
Lisa Min <lisajmin@gmail.com>
2017-05-12 20:16:43 -0700

    Assignment 20: List favorited posts

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Comment, type: :model do
 
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
-  let(:post) { create(:post, topic: topic, user: user) }
-  let(:comment) { Comment.create!(body: "Comment Body", post: post, user: user) }
+  let(:post) { create(:post) }
+  let(:comment) { create(:comment) }
 
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
@@ -14,13 +14,13 @@ RSpec.describe Comment, type: :model do
 
   describe "attritubes" do
     it "has a body attribute" do
-      expect(comment).to have_attributes(body: "Comment Body")
+      expect(comment).to have_attributes(body: comment.body)
     end
   end
 
   describe "after_create" do
     before do
-      @another_comment = Comment.new(body: 'Comment Body', post: post, user: user)
+      @another_comment = create(:comment, body: 'Comment Body', post: post, user: user)
     end
 
     it "sends an email to users who have favorited the post" do
